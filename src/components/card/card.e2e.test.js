1f712fd6c5bdae70a3e954d0b5a74743f7ae54ae
mas5592 <mas5592@yandex.ru>
2020-07-05 21:29:57 +0300

    Вносит правки

--- src/components/card/card.e2e.test.js
+++ src/components/card/card.e2e.test.js
@@ -2,27 +2,7 @@ import React from 'react';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Card from './card.jsx';
-
-const film = {
-  id: 1,
-  title: `Aviator`,
-  img: `img/aviator.jpg`,
-  release: 2004,
-  genre: `Dramas`,
-  poster: `https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/5d4dbb46-36e2-4892-a71d-fded5d0fe86d/x1000`,
-  coverBackground: `img/aviator.jpg`,
-  rating: {
-    score: 8.9,
-    level: `Very good`,
-    count: 240
-  },
-  description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
-  team: {
-    director: `Wes Andreson`,
-    starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other`
-  },
-  preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
-};
+import {film} from '../../data.js';
 
 Enzyme.configure({
   adapter: new Adapter(),
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,148 +1,138 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import MoviesList from '../movies-list/movies-list.jsx';
 import {SHOWING_FILMS_COUNT} from '../../data.js';
 
-class MovieCard extends PureComponent {
-  constructor(props) {
-    super(props);
+const MovieCard = (props) => {
 
-    this.filmClickHandler = this.filmClickHandler.bind(this);
-  }
+  const {film, films, onFilmClick} = props;
+  const {title, genre, coverBackground, poster, release, rating, description, team} = film;
+  const {score, level, count} = rating;
+  const {director, starring} = team;
 
-  filmClickHandler(film) {
-    this.props.onFilmClick(film);
-  }
+  return (
 
-  render() {
-    const {film, films} = this.props;
-    const {title, genre, coverBackground, poster, release, rating, description, team} = film;
-    const {score, level, count} = rating;
-    const {director, starring} = team;
-
-    return (
-
-      <React.Fragment>
-        <section className="movie-card movie-card--full">
-          <div className="movie-card__hero">
-            <div className="movie-card__bg">
-              <img src={coverBackground} alt={title} />
-            </div>
-
-            <h1 className="visually-hidden">WTW</h1>
-
-            <header className="page-header movie-card__head">
-              <div className="logo">
-                <a href="main.html" className="logo__link">
-                  <span className="logo__letter logo__letter--1">W</span>
-                  <span className="logo__letter logo__letter--2">T</span>
-                  <span className="logo__letter logo__letter--3">W</span>
-                </a>
-              </div>
-
-              <div className="user-block">
-                <div className="user-block__avatar">
-                  <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
-                </div>
-              </div>
-            </header>
-
-            <div className="movie-card__wrap">
-              <div className="movie-card__desc">
-                <h2 className="movie-card__title">{title}</h2>
-                <p className="movie-card__meta">
-                  <span className="movie-card__genre">{genre}</span>
-                  <span className="movie-card__year">{release}</span>
-                </p>
-
-                <div className="movie-card__buttons">
-                  <button className="btn btn--play movie-card__button" type="button">
-                    <svg viewBox="0 0 19 19" width={19} height={19}>
-                      <use xlinkHref="#play-s"></use>
-                    </svg>
-                    <span>Play</span>
-                  </button>
-                  <button className="btn btn--list movie-card__button" type="button">
-                    <svg viewBox="0 0 19 20" width={19} height={20}>
-                      <use xlinkHref="#add"></use>
-                    </svg>
-                    <span>My list</span>
-                  </button>
-                  <a href="add-review.html" className="btn movie-card__button">Add review</a>
-                </div>
-              </div>
-            </div>
+    <React.Fragment>
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img src={coverBackground} alt={title} />
           </div>
 
-          <div className="movie-card__wrap movie-card__translate-top">
-            <div className="movie-card__info">
-              <div className="movie-card__poster movie-card__poster--big">
-                <img src={poster} alt={title} width={218} height={327} />
-              </div>
+          <h1 className="visually-hidden">WTW</h1>
 
-              <div className="movie-card__desc">
-                <nav className="movie-nav movie-card__nav">
-                  <ul className="movie-nav__list">
-                    <li className="movie-nav__item movie-nav__item--active">
-                      <a href="#" className="movie-nav__link">Overview</a>
-                    </li>
-                    <li className="movie-nav__item">
-                      <a href="#" className="movie-nav__link">Details</a>
-                    </li>
-                    <li className="movie-nav__item">
-                      <a href="#" className="movie-nav__link">Reviews</a>
-                    </li>
-                  </ul>
-                </nav>
-
-                <div className="movie-rating">
-                  <div className="movie-rating__score">{score}</div>
-                  <p className="movie-rating__meta">
-                    <span className="movie-rating__level">{level}</span>
-                    <span className="movie-rating__count">{count} ratings</span>
-                  </p>
-                </div>
-
-                <div className="movie-card__text">
-                  <p>{description}</p>
-
-                  <p className="movie-card__director"><strong>Director: {director}</strong></p>
-
-                  <p className="movie-card__starring"><strong>Starring: {starring} and other</strong></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <div className="page-content">
-          <section className="catalog catalog--like-this">
-            <h2 className="catalog__title">More like this</h2>
-
-            <MoviesList
-              films={films.slice(0, SHOWING_FILMS_COUNT)}
-              onFilmClick={this.filmClickHandler}
-            />
-          </section>
-
-          <footer className="page-footer">
+          <header className="page-header movie-card__head">
             <div className="logo">
-              <a href="main.html" className="logo__link logo__link--light">
+              <a href="main.html" className="logo__link">
                 <span className="logo__letter logo__letter--1">W</span>
                 <span className="logo__letter logo__letter--2">T</span>
                 <span className="logo__letter logo__letter--3">W</span>
               </a>
             </div>
 
-            <div className="copyright">
-              <p>© 2019 What to watch Ltd.</p>
+            <div className="user-block">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
+              </div>
             </div>
-          </footer>
+          </header>
+
+          <div className="movie-card__wrap">
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{title}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{release}</span>
+              </p>
+
+              <div className="movie-card__buttons">
+                <button className="btn btn--play movie-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width={19} height={19}>
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list movie-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width={19} height={20}>
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+                <a href="add-review.html" className="btn movie-card__button">Add review</a>
+              </div>
+            </div>
+          </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+
+        <div className="movie-card__wrap movie-card__translate-top">
+          <div className="movie-card__info">
+            <div className="movie-card__poster movie-card__poster--big">
+              <img src={poster} alt={title} width={218} height={327} />
+            </div>
+
+            <div className="movie-card__desc">
+              <nav className="movie-nav movie-card__nav">
+                <ul className="movie-nav__list">
+                  <li className="movie-nav__item movie-nav__item--active">
+                    <a href="#" className="movie-nav__link">Overview</a>
+                  </li>
+                  <li className="movie-nav__item">
+                    <a href="#" className="movie-nav__link">Details</a>
+                  </li>
+                  <li className="movie-nav__item">
+                    <a href="#" className="movie-nav__link">Reviews</a>
+                  </li>
+                </ul>
+              </nav>
+
+              <div className="movie-rating">
+                <div className="movie-rating__score">{score}</div>
+                <p className="movie-rating__meta">
+                  <span className="movie-rating__level">{level}</span>
+                  <span className="movie-rating__count">{count} ratings</span>
+                </p>
+              </div>
+
+              <div className="movie-card__text">
+                <p>{description}</p>
+
+                <p className="movie-card__director"><strong>Director: {director}</strong></p>
+
+                <p className="movie-card__starring"><strong>Starring: {starring} and other</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+
+          <MoviesList
+            films={films.slice(0, SHOWING_FILMS_COUNT)}
+            onFilmClick={onFilmClick}
+          />
+        </section>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </React.Fragment>
+  );
+};
+
 
 MovieCard.propTypes = {
   film: PropTypes.shape({
