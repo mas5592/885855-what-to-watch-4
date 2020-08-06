7b90f5f51a557cb0ea0d21166c84e72fd57cbed0
mas5592 <mas5592@yandex.ru>
2020-08-06 23:45:11 +0300

    Вносит правки в иконки

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -60,8 +60,7 @@ const Main = ({card}) => {
               <div className="movie-card__buttons">
                 <Link to={`${AppRoute.VIDEO_PLAYER}/${id}`}
                   className="btn btn--play movie-card__button"
-                  type="button"
-                >
+                  type="button">
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"/>
                   </svg>
--- src/components/movie-card-hero/movie-card-hero.jsx
+++ src/components/movie-card-hero/movie-card-hero.jsx
@@ -41,7 +41,7 @@ const MovieCardHero = ({
               className="btn btn--play movie-card__button"
               to={`${AppRoute.VIDEO_PLAYER}/${activeFilm.id}`}
             >
-              <svg viewBox="0 0 19 19" width={19} height={19}>
+              <svg viewBox="0 0 19 19" width="19" height="19">
                 <use xlinkHref="#play-s"></use>
               </svg>
               <span>Play</span>
